30372b53aa9921bcdcdc474430acc10ef42a3fdb
rociomoscardi <rocioabrilmoscardi@gmail.com>
2025-05-02 18:48:14 -0300

    cambios

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -293,6 +293,11 @@
 
     <div class="container" data-aos="fade-up" data-aos-delay="100">
 
+     <!-- Section Title -->
+     <div class="container section-title" data-aos="fade-up">
+        <p><span>Dónde</span> <span class="description-title">encontrarnos</span></p>
+    </div><!-- End Section Title -->
+
       <div class="mb-5">
         <iframe style="width: 100%; height: 400px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen=""></iframe>
       </div><!-- End Google Maps -->
@@ -344,8 +349,7 @@
 
 
 
-    </div>
-
+   
   </section><!-- /Contact Section -->
 
 </main>
