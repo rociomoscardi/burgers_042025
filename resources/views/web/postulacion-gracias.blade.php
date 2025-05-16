15487c94e458aabc906c54a0801e4b3d6920d2c6
rociomoscardi <rocioabrilmoscardi@gmail.com>
2025-05-16 13:59:25 -0300

    cambios postulacion-gracias

--- resources/views/web/postulacion-gracias.blade.php
+++ resources/views/web/postulacion-gracias.blade.php
@@ -4,11 +4,11 @@
     <section id="gracias" class="gracias section">
         <div class="container" data-aos="fade-up">
             <div class="row">
-                <div class="col-6 py-5">
+                <div class="col-12 col-sm-6 py-3">
                     <p><span>¡Gracias! </span><br> Pronto estarás recibiendo noticias nuestras.</p>
                 </div>
-                <div class="col-6 py-5">
-                    <img src="web/img/logo1-img.png" alt="">
+                <div class="col-12 col-sm-6 py-3">
+                    <img src="web/img/postulacion-gracias-img.png" alt="">
                 </div>
             </div>
         </div>
